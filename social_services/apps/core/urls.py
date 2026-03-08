9bf08a3bb33b7b73a39a12cdd86f63fc52827b47
ecaucsn <ecaucsn@gmail.com>
2026-03-08 20:23:56 +0300

    Добавлен редирект с /executors/ на /executors/organizations/

--- social_services/apps/core/urls.py
+++ social_services/apps/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
     path('departments/<int:department_id>/print/', views.department_residents_print, name='department_residents_print'),
     path('departments/<int:department_id>/print-only/', views.department_residents_print_only, name='department_residents_print_only'),
     # Исполнители
+    path('executors/', RedirectView.as_view(pattern_name='organizations', permanent=False)),
     path('executors/organizations/', views.organization_list, name='organizations'),
     path('executors/organizations/<int:pk>/', views.organization_detail, name='organization_detail'),
     path('executors/employees/', views.employee_list, name='employees'),
